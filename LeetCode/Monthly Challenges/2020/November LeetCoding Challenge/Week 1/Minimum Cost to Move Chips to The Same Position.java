e39be3d804df9070e0f07ca819b5112540330eca
Faisal <hack2hire10@hack2hire.net>
2020-11-05 19:38:06 +0800

    Minimum Cost to Move Chips to The Same Position

---
+++ LeetCode/Monthly Challenges/2020/November LeetCoding Challenge/Week 1/Minimum Cost to Move Chips to The Same Position.java
@@ -0,0 +1,14 @@
+package practice.problems;
+
+class Solution {
+	public int minCostToMoveChips(int[] position) {
+		int odd = 0;
+		int even = 0;
+		for (int i = 0; i < position.length; i++)
+			if ((position[i] & 1) == 1)
+				odd++;
+			else
+				even++;
+		return Math.min(odd, even);
+	}
+}
